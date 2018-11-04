3fcdaa2d04610a8fb1fa29afaf7e85ace61dc1f9
Tony Hoang <hoangtrongminh304@gmail.com>
2018-11-04 17:47:48 +0700

    Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext content) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter2'),
-        ),
-        body: Center(
-          // child: Text('Hello World'),
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+//    final wordPair = WordPair.random();
+//    return Text(wordPair.asPascalCase);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
 }
